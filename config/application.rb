6d8456fd4846e2fae0b5338611930fb5abe5f32c
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-10-03 20:42:47 +0300

    add article

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Enbela
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :slim
+      g.test_framework  :rspec, fixture: false
+      g.scaffold_stylesheet false
+      g.stylesheets false
+      g.jbuilder false
+      g.assets false
+    end
   end
 end
