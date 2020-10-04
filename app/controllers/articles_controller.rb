a9bda52063f16da17a179d4cafb7959109da583f
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-10-04 22:11:09 +0300

    add user setup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,16 +24,13 @@ class ArticlesController < ApplicationController
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      flash.notice = 'Article was successfully created.'
+      redirect_to articles_path
+    else
+      render :new
     end
   end
 
