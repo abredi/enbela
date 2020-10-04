2b8e10a74fc4a19ac70dd58a6210c7e3bd8900df
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-10-05 02:13:16 +0300

    add article image feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  has_attached_file :image,
+                    storage: :cloudinary, path: 'uploaded/:class/:attachment/:id/:style_:filename'
+  validates_attachment_content_type :image, content_type: %w[image/jpeg image/gif image/png]
+  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
+  validates_presence_of :title
+  validates_presence_of :text
 end
