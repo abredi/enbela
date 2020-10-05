b646a32a57f6ef00ee504a8a3f66a5dfffb82c21
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-10-05 13:37:42 +0300

    add associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,25 @@ class Article < ApplicationRecord
                     storage: :cloudinary, path: 'uploaded/:class/:attachment/:id/:style_:filename'
   validates_attachment_content_type :image, content_type: %w[image/jpeg image/gif image/png]
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
+
+  belongs_to :user
+  belongs_to :category
+  has_many :votes
   validates_presence_of :title
   validates_presence_of :text
+
+  scope :featured, lambda {
+    Article.joins(:votes).select('articles.*, count(article_id) AS count_all')
+           .group('article_id, title, text, articles.id')
+           .order(count_all: :desc).limit(1)
+  }
+  scope :categories, lambda {
+    Article.joins(:category).select('articles.*, priority, name')
+           .distinct.order(priority: :desc).to_a
+  }
+
+  scope :articles, lambda { |id|
+    Article.joins(:category, :user).select('users.*, articles.*, title, user_id, text, category_id')
+           .where(categories: {id: id}).order(:updated_at).to_a
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :name
   validates_length_of :name, within: 3..20
+
+  has_many :articles
+  has_many :votes
 end
