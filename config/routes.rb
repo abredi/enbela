c6d604705ec420855b9d51dd78b8d4bae433eb7a
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-10-05 12:45:59 +0300

    add session feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions
   resources :articles, only: %i[index create update delete show edit new]
   root to: 'articles#index'
 end
